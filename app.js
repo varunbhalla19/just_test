51029cfd6260fd00ac5317ffff5ea4586b9e16e3
Varun <varun@varun-HP-Ubuntu-18.04>
2020-09-30 16:35:21 +0530

    Changes to Test 2

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ console.log("For Master Branch");
 
 console.log("For Master Branch Again!")
 
-console.log("For Test Branch");
-
-console.log("For Test Branch Again!")
+console.log("For Master Branch Again Again!")
